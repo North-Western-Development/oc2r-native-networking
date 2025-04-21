e3539ce761e9aa75177fa8989ca9a4ef4a8972a0
Un1q32 <joey.t.reinhart@gmail.com>
2025-04-20 20:56:54 -0400

    gnu only :(

--- build.sh
+++ build.sh
@@ -37,8 +37,8 @@ if ! [ -d zig ]; then
     mv zig-* zig
 fi
 
-zig/zig cc $CFLAGS -Ijni-headers oc2rnet.c -static -shared -target x86_64-linux-musl -o build/liboc2rnet-linux-x86_64.so
-zig/zig cc $CFLAGS -Ijni-headers oc2rnet.c -static -shared -target aarch64-linux-musl -o build/liboc2rnet-linux-arm64.so
+zig/zig cc $CFLAGS -Ijni-headers oc2rnet.c -shared -target x86_64-linux-gnu -o build/liboc2rnet-linux-x86_64.so
+zig/zig cc $CFLAGS -Ijni-headers oc2rnet.c -shared -target aarch64-linux-gnu -o build/liboc2rnet-linux-arm64.so
 zig/zig cc $CFLAGS -Ijni-headers oc2rnet.c -shared -target x86_64-windows-gnu -o build/oc2rnet-x86_64.dll -licmp
 zig/zig cc $CFLAGS -Ijni-headers oc2rnet.c -shared -target aarch64-windows-gnu -o build/oc2rnet-arm64.dll -licmp
 
